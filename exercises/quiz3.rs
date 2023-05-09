e4a01573f430d4e4f779705f30f3e9ee6da206f0
Antti Pham <antti.pham@tuni.fi>
2023-05-09 14:38:17 +0300

    done

--- exercises/quiz3.rs
+++ exercises/quiz3.rs
@@ -14,7 +14,6 @@
 
 // Execute `rustlings hint quiz3` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
 
 pub struct ReportCard {
     pub grade: f32,
@@ -26,6 +25,14 @@ impl ReportCard {
     pub fn print(&self) -> String {
         format!("{} ({}) - achieved a grade of {}",
             &self.student_name, &self.student_age, &self.grade)
+    }
+    pub fn print_alph(&self) -> String {
+        let alph = match self.grade {
+            x if x > 2.0 => "A+",
+            _ => "F-"
+        };
+        format!("{} ({}) - achieved a grade of {}",
+            &self.student_name, &self.student_age, alph)
     }
 }
 
@@ -55,7 +62,7 @@ mod tests {
             student_age: 11,
         };
         assert_eq!(
-            report_card.print(),
+            report_card.print_alph(),
             "Gary Plotter (11) - achieved a grade of A+"
         );
     }
